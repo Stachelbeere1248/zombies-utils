d7289148e00a11688ebb5489f0ec63a376e8631c
GitHub Action <action@github.com>
2023-09-21 12:50:28 +0000

    Initialize template repository proper
    Set package name to com.github.stachelbeere1248.zombiesutils
    Set project name to zombies-utils
    Set modid to zombiesutils

--- src/main/java/com/github/stachelbeere1248/zombiesutils/ExampleMod.java
+++ src/main/java/com/github/stachelbeere1248/zombiesutils/ExampleMod.java
@@ -1,10 +1,10 @@
-package com.example;
+package com.github.stachelbeere1248.zombiesutils;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "examplemod", useMetadata=true)
+@Mod(modid = "zombiesutils", useMetadata=true)
 public class ExampleMod {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
--- src/main/java/com/github/stachelbeere1248/zombiesutils/mixin/MixinGuiMainMenu.java
+++ src/main/java/com/github/stachelbeere1248/zombiesutils/mixin/MixinGuiMainMenu.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package com.github.stachelbeere1248.zombiesutils.mixin;
 
 import net.minecraft.client.gui.GuiMainMenu;
 import org.spongepowered.asm.mixin.Mixin;
