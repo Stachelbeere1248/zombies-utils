d8b44b124f69a3d4045b796ebe33ae8cd49fef0a
stachelbeere1248 <moritzrossbacher@gmail.com>
2024-12-13 19:03:23 +0100

    initial commit

---
+++ src/client/java/com/github/stachelbeere1248/zombiesutils/ZombiesUtilsClient.java
@@ -0,0 +1,10 @@
+package com.github.stachelbeere1248.zombiesutils;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ZombiesUtilsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
